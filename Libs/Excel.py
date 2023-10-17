6cb56ea14371939e4b5b6f7deefc17901341d0e9
Hippalectryon <hippalectryon@protonmail.com>
2023-10-17 15:29:36 +0200

    simplify tcomp columns

--- Libs/Excel.py
+++ Libs/Excel.py
@@ -222,27 +222,30 @@ def save_to_file(df_raw: pd.DataFrame, output_path: Path) -> None:
         tcomp_vertical_offset = synth_vertical_offset + 10
         # Input and description
         sheet_formatted.write(tcomp_vertical_offset, tcomp_column, "Pour un trajet précis", workbook.add_format({"bold": True}))
-        sheet_formatted.write(tcomp_vertical_offset, tcomp_column + 3, "Ville départ:")
-        sheet_formatted.write(tcomp_vertical_offset, tcomp_column + 4, "Paris")
-        sheet_formatted.write(tcomp_vertical_offset, tcomp_column + 5, "Pays départ:")
-        sheet_formatted.write(tcomp_vertical_offset, tcomp_column + 6, "FR")
-        sheet_formatted.write(tcomp_vertical_offset + 1, tcomp_column + 3, "Ville arrivée:")
-        sheet_formatted.write(tcomp_vertical_offset + 1, tcomp_column + 4, "Vienne")
-        sheet_formatted.write(tcomp_vertical_offset + 1, tcomp_column + 5, "Pays arrivée:")
-        sheet_formatted.write(tcomp_vertical_offset + 1, tcomp_column + 6, "AT")
-        tcomp_vertical_offset += 2
+        sheet_formatted.write(
+            tcomp_vertical_offset, tcomp_column + 3, 'Utilisez "*" pour indiquer une valeur quelconque', workbook.add_format({"italic": True})
+        )
+        sheet_formatted.write(tcomp_vertical_offset + 1, tcomp_column + 3, "Ville 1:")
+        sheet_formatted.write(tcomp_vertical_offset + 1, tcomp_column + 4, "Paris")
+        sheet_formatted.write(tcomp_vertical_offset + 1, tcomp_column + 5, "Pays 1:")
+        sheet_formatted.write(tcomp_vertical_offset + 1, tcomp_column + 6, "FR")
+        sheet_formatted.write(tcomp_vertical_offset + 2, tcomp_column + 3, "Ville 2:")
+        sheet_formatted.write(tcomp_vertical_offset + 2, tcomp_column + 4, "Vienne")
+        sheet_formatted.write(tcomp_vertical_offset + 2, tcomp_column + 5, "Pays 2:")
+        sheet_formatted.write(tcomp_vertical_offset + 2, tcomp_column + 6, "AT")
+        tcomp_vertical_offset += 3
         # First column
         transport_format = workbook.add_format({"bg_color": "#ffc000", **f_border_align_center})
-        synth_transports = ["Voiture", "Train", "Vol court", "Vol moyen", "Vol long"]
+        tcomp_transports = ["Voiture", "Train", "Vol"]
         sheet_formatted.set_column(tcomp_column, tcomp_column, 12)
-        for i, s in enumerate(synth_transports):
+        for i, s in enumerate(tcomp_transports):
             sheet_formatted.write(tcomp_vertical_offset + i + 1, tcomp_column, s, transport_format)
         total_format = workbook.add_format({"border": 1, "align": "center", "valign": "vcenter"})
-        sheet_formatted.write(tcomp_vertical_offset + 1 + len(synth_transports), tcomp_column, "Total", total_format)
+        sheet_formatted.write(tcomp_vertical_offset + 1 + len(tcomp_transports), tcomp_column, "Total", total_format)
         # Second column
         transport_id_format = workbook.add_format({"bg_color": "#acb20c", **f_border_align_center})
-        synth_transports_ids = ["ID", "car", "train *", "plane (short)", "plane (med)", "plane (long)"]
-        for i, s in enumerate(synth_transports_ids):
+        tcomp_transports_ids = ["ID", "car", "train *", "plane*"]
+        for i, s in enumerate(tcomp_transports_ids):
             sheet_formatted.write(tcomp_vertical_offset + i, tcomp_column + 1, s, transport_id_format)
         sheet_formatted.set_column(tcomp_column + 1, tcomp_column + 1, 15, None, {"hidden": True})  # Hide by default
         # Headers
@@ -264,29 +267,30 @@ def save_to_file(df_raw: pd.DataFrame, output_path: Path) -> None:
         tcomp_column_arrival_city = index_to_column(columns_formatted.index(Enm.COL_ARRIVAL_CITY))
         tcomp_column_arrival_country = index_to_column(columns_formatted.index(Enm.COL_ARRIVAL_COUNTRYCODE))
         column_transport_id = index_to_column(tcomp_column + 1)
-        tcomp_condition = f"${tcomp_column_departure_city}:${tcomp_column_departure_city}, ${index_to_column(tcomp_column+4)}${tcomp_vertical_offset-1}, ${tcomp_column_departure_country}:${tcomp_column_departure_country}, ${index_to_column(tcomp_column+6)}${tcomp_vertical_offset-1}, ${tcomp_column_arrival_city}:${tcomp_column_arrival_city}, ${index_to_column(tcomp_column+4)}${tcomp_vertical_offset}, ${tcomp_column_arrival_country}:${tcomp_column_arrival_country}, ${index_to_column(tcomp_column+6)}${tcomp_vertical_offset}"
-        for i, s in enumerate(synth_transports):  # N_trips
+        tcomp_condition_1 = f"${tcomp_column_departure_city}:${tcomp_column_departure_city}, ${index_to_column(tcomp_column+4)}${tcomp_vertical_offset-1}, ${tcomp_column_departure_country}:${tcomp_column_departure_country}, ${index_to_column(tcomp_column+6)}${tcomp_vertical_offset-1}, ${tcomp_column_arrival_city}:${tcomp_column_arrival_city}, ${index_to_column(tcomp_column+4)}${tcomp_vertical_offset}, ${tcomp_column_arrival_country}:${tcomp_column_arrival_country}, ${index_to_column(tcomp_column+6)}${tcomp_vertical_offset}"
+        tcomp_condition_2 = f"${tcomp_column_arrival_city}:${tcomp_column_arrival_city}, ${index_to_column(tcomp_column+4)}${tcomp_vertical_offset-1}, ${tcomp_column_arrival_country}:${tcomp_column_arrival_country}, ${index_to_column(tcomp_column+6)}${tcomp_vertical_offset-1}, ${tcomp_column_departure_city}:${tcomp_column_departure_city}, ${index_to_column(tcomp_column+4)}${tcomp_vertical_offset}, ${tcomp_column_departure_country}:${tcomp_column_departure_country}, ${index_to_column(tcomp_column+6)}${tcomp_vertical_offset}"
+        for i, s in enumerate(tcomp_transports):  # N_trips
             sheet_formatted.write_formula(
                 tcomp_vertical_offset + i + 1,
                 tcomp_column + 2,
-                f"COUNTIFS(${tcomp_column_transport}:${tcomp_column_transport}, {column_transport_id}{tcomp_vertical_offset + 2 + i}, {tcomp_condition})",
+                f"COUNTIFS(${tcomp_column_transport}:${tcomp_column_transport}, {column_transport_id}{tcomp_vertical_offset + 2 + i}, {tcomp_condition_1})+COUNTIFS(${tcomp_column_transport}:${tcomp_column_transport}, {column_transport_id}{tcomp_vertical_offset + 2 + i}, {tcomp_condition_2})",
                 synth_cell_format_int,
             )
         for j, col in enumerate([tcomp_column_distance, tcomp_column_emission, tcomp_column_uncertainty]):  # other columns
-            for i, transp in enumerate(synth_transports):
+            for i, transp in enumerate(tcomp_transports):
                 sheet_formatted.write_formula(
                     tcomp_vertical_offset + i + 1,
                     tcomp_column + 3 + j,
-                    f"SUMIFS({col}:{col}, ${tcomp_column_transport}:${tcomp_column_transport}, ${column_transport_id}{tcomp_vertical_offset + 2 + i}, {tcomp_condition})/1000",
+                    f"(SUMIFS({col}:{col}, ${tcomp_column_transport}:${tcomp_column_transport}, ${column_transport_id}{tcomp_vertical_offset + 2 + i}, {tcomp_condition_1})+SUMIFS({col}:{col}, ${tcomp_column_transport}:${tcomp_column_transport}, ${column_transport_id}{tcomp_vertical_offset + 2 + i}, {tcomp_condition_2}))/1000",
                     synth_cell_format,
                 )
         synth_total_format = workbook.add_format({"bg_color": "#ffe699", **f_border_align_center, "num_format": "0.0"})
         for j in range(4):  # totals
             col = tcomp_column + 2 + j
             sheet_formatted.write_formula(
-                tcomp_vertical_offset + 6,
+                tcomp_vertical_offset + 4,
                 tcomp_column + 2 + j,
-                f"SUM({index_to_column(col)}{tcomp_vertical_offset+2}:{index_to_column(col)}{tcomp_vertical_offset+6})",
+                f"SUM({index_to_column(col)}{tcomp_vertical_offset+2}:{index_to_column(col)}{tcomp_vertical_offset+4})",
                 synth_total_format
                 if j > 0
                 else workbook.add_format({"bg_color": "#ffe699", "border": 1, "align": "center", "valign": "vcenter", "num_format": "0"}),
