5a84a0a81cf9ab8f645e62a7d494387d277cb318
Hippalectryon <hippalectryon@protonmail.com>
2023-12-02 09:44:14 +0100

    compute twoway emissions

--- Libs/VisualMap.py
+++ Libs/VisualMap.py
@@ -25,11 +25,11 @@ def _prepare_data(df: pd.DataFrame) -> pd.DataFrame:
         lambda row: row["arrival_loc"] if check_distance_to_paris(row["departure_loc"]) else row["departure_loc"],
         axis=1,
     )
-    df["emissions_oneway"] = df["co2e_emissions_kg"] / (2 * (df["round_trip"] == "oui") + 1)
+    df["emissions_twoway"] = df["co2e_emissions_kg"] * (1 * (df["round_trip"] == "non") + 1)
 
     new_df = (
         df.groupby("notparis_address")
-        .agg(total_emissions_oneway=("emissions_oneway", "sum"), single_emissions_oneway=("emissions_oneway", "mean"))
+        .agg(total_emissions_twoway=("emissions_twoway", "sum"), single_emissions_twoway=("emissions_twoway", "mean"))
         .reset_index()
     )
 
@@ -40,21 +40,21 @@ def generate_visual_map(df: pd.DataFrame, output_path: Path) -> None:
     """create an image map summarizing the emissions from Paris"""
     df = _prepare_data(df.copy())
     # kg -> t
-    df["single_emissions_oneway"] /= 1e3
+    df["single_emissions_twoway"] /= 1e3
 
     fig, ax = plt.subplots(figsize=(10, 8))
     m = Basemap(projection="mill", llcrnrlat=-90, urcrnrlat=90, llcrnrlon=-180, urcrnrlon=180, resolution="c", ax=ax)
     m.drawcoastlines()
     m.drawcountries()
     cmap = ListedColormap(plt.get_cmap("cool")(np.linspace(0, 1, 5)))
-    normalize = Normalize(vmin=df["single_emissions_oneway"].min(), vmax=df["single_emissions_oneway"].max())
+    normalize = Normalize(vmin=df["single_emissions_twoway"].min(), vmax=df["single_emissions_twoway"].max())
 
     # Plot circles on the map
-    df["circle_size"] = df["total_emissions_oneway"] / df["total_emissions_oneway"].max()
+    df["circle_size"] = df["total_emissions_twoway"] / df["total_emissions_twoway"].max()
     max_radius = 1e2
     for index, row in df.iterrows():
         x, y = m(row["notparis_address"].longitude, row["notparis_address"].latitude)
-        m.plot(x, y, "o", markersize=max_radius * row["circle_size"], color=cmap(normalize(row["single_emissions_oneway"])), alpha=0.6)
+        m.plot(x, y, "o", markersize=max_radius * row["circle_size"], color=cmap(normalize(row["single_emissions_twoway"])), alpha=0.6)
         m.plot(x, y, "o", markersize=min(2, max_radius * row["circle_size"]), color="black", zorder=999)
     plt.title("Emissions from Paris by destination")
 
@@ -62,7 +62,7 @@ def generate_visual_map(df: pd.DataFrame, output_path: Path) -> None:
     sm = plt.cm.ScalarMappable(cmap=cmap, norm=normalize)
     sm.set_array([])  # Dummy array for the colorbar
     cbar = plt.colorbar(sm, ax=ax, orientation="vertical", shrink=0.5)
-    cbar.set_label("Emissions per trip (tCO2e)")
+    cbar.set_label("Emissions per round trip (tCO2e)")
 
     # Add Europe zoom encart
     axins = inset_axes(ax, width="50%", height="45%", loc="lower left", borderpad=0, bbox_to_anchor=(-0.1, 0.01, 1, 1), bbox_transform=ax.transAxes)
@@ -71,7 +71,7 @@ def generate_visual_map(df: pd.DataFrame, output_path: Path) -> None:
     m_europe.drawcountries()
     for index, row in df.iterrows():
         x, y = m_europe(row["notparis_address"].longitude, row["notparis_address"].latitude)
-        m_europe.plot(x, y, "o", markersize=max_radius * row["circle_size"], color=cmap(normalize(row["single_emissions_oneway"])), alpha=0.6)
+        m_europe.plot(x, y, "o", markersize=max_radius * row["circle_size"], color=cmap(normalize(row["single_emissions_twoway"])), alpha=0.6)
         m_europe.plot(x, y, "o", markersize=min(2, max_radius * row["circle_size"]), color="black", zorder=999)
 
     # Add legend with custom handler
